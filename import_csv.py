31f46d9b94ead16b8fe138b090cce16864b4f7ee
chaitanyagulage <chaitanyagulage6@gmail.com>
2026-04-20 16:18:25 +0530

    Add files via upload

--- import_csv.py
+++ import_csv.py
@@ -1,10 +1,28 @@
-import csv
+#Demonstration of file handling operation in python 
+file = open("sample.txt","w")
+file.write("hello this is the first line in the file.\n")
+file.write("python file handling demonstration.\n")
+file.close()
 
-row_count = 0
-with open("Book1.csv", "r") as file:
-    reader = csv.reader(file)
+print("data written successfully.\n")
 
-    for row in reader:
-        print(row)
-        row_count += 1
-print("Total number of rows:", row_count
+file = open("sample.txt", "r")
+print("reading file contents:")
+content =file.read()
+print(content)
+file.close()
+
+file=open("sample.txt","a")
+file.write("this line is added later using append mode.\n")
+file.close()
+
+print("data appended successfully.\n")
+
+#4.Read the file agin to see update
+file=open("sample.txt","r")
+print("Updated file contents:")
+updated_content=file.read()
+update_content=file.read()
+
+print(updated_content)
+file.close()
