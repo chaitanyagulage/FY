65c6b271475a3ba9e7a847dcec09812504ff684b
chaitanyagulage <chaitanyagulage6@gmail.com>
2026-04-21 21:43:16 +0530

    Delete queue.py

--- queue.py
+++
@@ -1,9 +0,0 @@
-import math
-def circle_area (radius):
-    return math.pi*radius*radius
-
-def rectangle_area(lenght,width):
-    return lenght*width
-
-def triangle_area (base,heigth):
-    return 0.5*base*heigth
